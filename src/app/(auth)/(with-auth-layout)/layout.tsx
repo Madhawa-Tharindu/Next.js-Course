4af96bdf65262793525a92217277a4a04d3a90ce
Madhawa-Tharindu <madhawath@gmail.com>
2025-05-06 10:16:39 +0530

    learnt about mapping

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,13 +1,17 @@
-export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <>
+// src/app/(auth)/(with-auth-layout)/layout.tsx
+
+import AuthNavbar from "@/components/AuthNavBar";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <AuthNavbar />
       {children}
       <h2>Inner Layout</h2>
-      </>
-    );
-  }
-  
+    </>
+  );
+}
